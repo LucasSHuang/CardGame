e9facec1d0563de2b3cdd00ff513d547869e9f81
Lucas Huang <mba@Lucass-MacBook-Air-3.local>
2024-11-18 09:42:51 -0800

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,45 @@
+public class Card {
+
+    // Instance Variables
+    private String rank;
+    private String suit;
+    private int value;
+
+    // Constructor
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    // Getters and setters
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
