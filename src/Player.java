03367fb0d07f279ecc5c341f2c5218b212f23925
Lucas Huang <lucas.huang@menloschool.org>
2024-11-19 15:51:12 -0800

    Finished Player Class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,60 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    // Instance variables
+    private ArrayList<Card> hand;
+    private int points;
+    private String name;
+
+    // Constructors
+    public Player(ArrayList<Card> hand, String name) {
+        this.hand = new ArrayList<Card>();
+        this.hand = hand;
+        this.points = 0;
+        this.name = name;
+    }
+
+    public Player(String name) {
+        this.hand = new ArrayList<Card>();
+        this.points = 0;
+        this.name = name;
+    }
+
+    // Getters and setters
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    // Adding points and adding cards
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    // String
+    public String toString() {
+        return this.name + " has " + this.points + " points\n" + this.name + "'s cards: " + hand;
+    }
+
+
 }
