d51a5ad572c861e8e5a49ac3af3cb46d8c95e564
Lucas Huang <lucas.huang@menloschool.org>
2025-02-07 12:57:10 -0800

    Created GameViewer class

---
+++ src/GameViewer.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+    public class GameViewer extends JFrame {
+        public final int WINDOW_WIDTH = 1000;
+        public final int WINDOW_HEIGHT = 1500;
+        public final int TITLE_BAR_HEIGHT = 23;
+        private Game game;
+    }
